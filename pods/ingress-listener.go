058d9b1ba9c5eb683c5d0fe30bc9b8b218a6f748
Jacqueline Abdo <jfabdo@gmail.com>
2020-09-03 09:41:48 -0600

    Added subscription

--- pods/ingress-listener.go
+++ pods/ingress-listener.go
@@ -6,8 +6,20 @@ import (
 	"github.com/gomodule/redigo/redis"
 )
 
-func main() {
-	connection = utils.Connection()
+//PostMessage is the format for committing to the redis post queue
+type PostMessage struct {
+	BID     string				`redis:bid` 	//Browser ID
+	LID     string  			`redis:lid`     //Login ID
+	SID     string  			`redis:sid`     //Session ID
+	command string  			`redis:command` //What this message is supposed to do, e.g. report, post comment, etc.
+	comment string  			`redis:comment` //What this message is acting on, i.e. the URI
+	body    string  			`redis:body`    //The comment, report, etc.
+	writer  http.ResponseWriter 				//Where information is being returned to
+}
 
-	
+func main() {
+	client = utils.Connection()
+	sub := client.Subscribe(queryResp)
+	iface, err := sub.Receive()
+	ch := sub.Channel()
 }
